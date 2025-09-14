63c0f9ffc9d48b72e7eeb9eaa50df5b53935277f
Garrett-Webster <garrettreeds@gmail.com>
2025-09-13 18:17:00 -0600

    Add the King Moves

--- shared/src/main/java/chess/MoveCalculators/KingMoveCalculator.java
+++ shared/src/main/java/chess/MoveCalculators/KingMoveCalculator.java
@@ -6,5 +6,31 @@ import chess.ChessPosition;
 public class KingMoveCalculator extends MoveCalculator{
     KingMoveCalculator(ChessPosition position, ChessBoard board) {
         super(position, board);
+        addKingMoves();
+    }
+
+    private void addKingMoves() {
+        int[][] kingModifiers = {
+                {1,-1},
+                {1,0},
+                {1,1},
+                {0, 1},
+                {0,-1},
+                {-1,-1},
+                {-1,0},
+                {-1,1}
+        };
+
+        int row = this.position.getRow();
+        int col = this.position.getColumn();
+
+        for (int[] mod: kingModifiers) {
+            int newRow = row+mod[0];
+            int newCol = col+mod[1];
+
+            if(newRow > 0 && newRow < 9 && newCol > 0 && newCol <9) {
+                checkCollisionAndAddMove(row+mod[0], col+mod[1]);
+            }
+        }
     }
 }
