58d3c8e753289c5769ec38c30ea6aeb48d64a2e0
Garrett-Webster <garrettreeds@gmail.com>
2025-10-25 19:51:47 -0600

    Implement the create and get user functions in DatabaseUserDao

--- server/src/main/java/dataaccess/database/DatabaseUserDao.java
+++ server/src/main/java/dataaccess/database/DatabaseUserDao.java
@@ -1,16 +1,25 @@
 package dataaccess.database;
 
+import dataaccess.DataAccessException;
 import dataaccess.UserDao;
 import model.UserData;
 
 public class DatabaseUserDao extends UserDao {
     @Override
-    public void createUser(UserData userData) {
+    public void createUser(UserData userData) throws DataAccessException {
+        String sql_statement = "INSERT INTO users (username, password, email) VALUES(?,?,?)";
+        executeCommand(sql_statement, userData.username(), userData.password(), userData.email());
     }
 
     @Override
-    public UserData getUser(String username) {
-        return null;
+    public UserData getUser(String username) throws DataAccessException {
+        String sql_statement = "SELECT username, password, email FROM users WHERE username = ?";
+
+        return executeQueryAndGetOne(sql_statement, results -> new UserData(
+                results.getString("username"),
+                results.getString("password"),
+                results.getString("email")
+        ), username);
     }
 
     @Override
@@ -20,5 +29,7 @@ public class DatabaseUserDao extends UserDao {
 
     @Override
     public void clear() {
+//        String sql_statement = "TRUNCATE TABLE users";
+//        executeCommand(sql_statement);
     }
 }
