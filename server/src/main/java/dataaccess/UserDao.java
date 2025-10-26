6c689e841acc91c4b6f34344aac538fdc75e2dec
Garrett-Webster <garrettreeds@gmail.com>
2025-10-25 19:48:20 -0600

    Clean up the UserDao

--- server/src/main/java/dataaccess/UserDao.java
+++ server/src/main/java/dataaccess/UserDao.java
@@ -4,20 +4,7 @@ import model.UserData;
 
 public abstract class UserDao extends Dao {
     public abstract void createUser(UserData userData) throws DataAccessException;
-
-//    public UserData getUser(int userId) throws DataAccessException;
-
     public abstract UserData getUser(String username) throws DataAccessException;
-
     public abstract boolean validateWithPassword(String username, String password);
-//
-//    public UserData getUser(String userName);
-//
-//    public void updateUser(UserData userData) throws DataAccessException;
-//
-//    public void deleteUser(int userId) throws DataAccessException;
-//
-//    public UserData validateUser(String username, String password);
-//
-public abstract void clear();
+    public abstract void clear();
 }
