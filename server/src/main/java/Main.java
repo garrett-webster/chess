00f58592467d3ce8aaa5d27da7f636562f492082
Garrett-Webster <garrettreeds@gmail.com>
2025-10-29 17:42:34 -0600

    Hacky things to make the autograder happy.

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,14 +1,9 @@
-import dataaccess.DataAccessException;
 import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         Server server = new Server();
-        try{
-            server.run(8080);
-            System.out.println("♕ 240 Chess Server");
-        } catch (DataAccessException e) {
-            System.out.println("Failed to start the database");
-        }
+        server.run(8080);
+        System.out.println("♕ 240 Chess Server");
     }
 }
