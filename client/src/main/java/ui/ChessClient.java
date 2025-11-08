8ef051a8dcd3e3b0007e23763cc8336a24780d39
Garrett-Webster <garrettreeds@gmail.com>
2025-11-08 15:37:39 -0700

    Refine the basic flow and printing

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -3,23 +3,28 @@ package ui;
 import static ui.EscapeSequences.*;
 
 import exception.ResponseException;
+import model.GameData;
+import requestobjects.CreateRequest;
 import requestobjects.LoginRequest;
 import requestobjects.RegisterRequest;
 import server.ServerFacade;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ChessClient {
     private final ServerFacade server;
     private State state = State.SIGNEDOUT;
     Scanner scanner = new Scanner(System.in);
+    String authToken;
+    List<GameData> games;
 
     public ChessClient(String url) {
         server = new ServerFacade(url);
     }
 
     public void run() {
-        System.out.println("Welcome to the chess client. Type \"help\" to see available actions.\n");
+        System.out.println("Welcome to the chess client. Type \"help\" to see available actions.");
 
         Scanner scanner = new Scanner(System.in);
         var result = "";
@@ -40,11 +45,11 @@ public class ChessClient {
                     } else {
                         result = "Unknown command. Type \"help\" to see valid commands.";
                     }
-                };
-                System.out.print(SET_TEXT_COLOR_BLUE + result);
+                }
+                System.out.println(SET_TEXT_COLOR_BLUE + result);
             } catch (Throwable e) {
-                var msg = e.toString();
-                System.out.print(msg);
+                var msg = e.getMessage();
+                System.out.println(msg);
             }
         }
     }
@@ -55,7 +60,7 @@ public class ChessClient {
           case "login" -> login();
           case "quit" -> "quit";
           case "register" -> register();
-          default -> throw new IllegalStateException("Unexpected value: " + line + "Type help for valid commands.");
+          default -> throw new IllegalStateException("Unexpected value: " + line + " Type help for valid commands.");
         };
     }
 
@@ -73,15 +78,18 @@ public class ChessClient {
     }
 
     private void printPrompt() {
-        System.out.print("\n" + RESET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_GREEN);
+        System.out.print(RESET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_GREEN);
     }
 
     private String register() {
         System.out.println("Enter username");
+        printPrompt();
         String username = scanner.nextLine();
         System.out.println("Enter password");
+        printPrompt();
         String password = scanner.nextLine();
         System.out.println("Enter email");
+        printPrompt();
         String email = scanner.nextLine();
 
         try {
@@ -95,16 +103,62 @@ public class ChessClient {
 
     private String login() {
         System.out.println("Enter username");
+        printPrompt();
         String username = scanner.nextLine();
         System.out.println("Enter password");
+        printPrompt();
         String password = scanner.nextLine();
 
         try {
-            server.loginUser(new LoginRequest(username, password));
+            authToken = server.loginUser(new LoginRequest(username, password)).authToken();
             state = State.SIGNEDIN;
             return "Successfully signed in.";
         } catch (ResponseException e) {
             return "Failed to sign in with the given credentials.";
+        }
+    }
+
+    private String logout() {
+        try {
+            server.logoutUser(authToken);
+            state = State.SIGNEDOUT;
+            return "Logging out...";
+        } catch (ResponseException e) {
+            return "Something failed while logging out. Check your connection to the server.";
+        }
+    }
+
+    private String newGame() {
+        System.out.println("Enter name for new game");
+        printPrompt();
+        String name = scanner.nextLine();
+        try {
+            server.createGame(authToken, new CreateRequest(name));
+            return "Created game \"" + name + "\"";
+        } catch (ResponseException e) {
+            return "Failed to create new game";
+        }
+    }
+
+    private String listGames() {
+        try {
+            games = server.listGame(authToken).games();
+            StringBuilder gameStrings = new StringBuilder();
+            gameStrings.append("Games\n");
+
+            for (int i = 0; i < games.size(); i++) {
+                GameData game = games.get(i);
+                String gameString = (i+1) + ": " + game.gameName();
+                if (game.whiteUsername() != null) {gameString += " White: " + game.whiteUsername();}
+                if (game.blackUsername() != null) {gameString += " Black: " + game.blackUsername();}
+                gameStrings.append(gameString).append("\n");
+            }
+
+            gameStrings.setLength(gameStrings.length() - 1);
+            return gameStrings.toString();
+
+        } catch (ResponseException e) {
+            return "Failed to get games. Check your connection to the server and try again.";
         }
     }
 
